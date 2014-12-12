71d6474529f39f2def2d02ef392f99a53e343a10
Carl Smith <carl.input@gmail.com>
2014-12-12 19:50:02 +0000

    First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+This is an example solution to the *Model the Bicycle Industry* assignment in [Thinkful's
+*Programming in Python* course][1], (`PIP001(v3) 1.3.4`).
+
+[1]: https://wow.thinkful.com/courses/learn-python-online
+
---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+from bicycles import Bicycle, BikeShop, Customer
+
+# First create a list of Bikes, then create a BikeShop, stocking it
+# with the Bikes...
+
+bikes = [
+	Bicycle("Rock Hopper", 75, 100), Bicycle("Dirt Jumper", 70, 150),
+	Bicycle("Speed Demon", 50, 250), Bicycle("Mountin Top", 90, 350),
+	Bicycle("Road Master", 65, 100), Bicycle("Ghetto King", 75, 550)
+	]
+
+shop = BikeShop("Surplus Cycles", 20, bikes)
+
+# Now, create a list of Customers, then iterate over them, printing
+# the Customer's name and the Bikes that they can afford...
+
+customers = [Customer("Ali", 200), Customer("Bob", 500), Customer("Caz", 1000)]
+
+for customer in customers:
+
+	bikes = ", ".join( bike.model for bike in shop.filter(customer.fund) )
+	print customer.name, "|", bikes
+
+# Print the BikeShop before making any sales...
+
+print shop
+
+# Iterate over the customers, selling each a Bike, then using a template,
+# print who the customer is, what they bought, what it cost, and how much
+# they have left over...
+
+template = "{0} bought the {1} at ${2}, and they have ${3} left over."
+
+for customer in customers:
+	
+	affordables = shop.filter(customer.fund)
+	shop.sell(random.choice(affordables), customer)
+	
+	print template.format(
+		customer.name, customer.bike.model,
+		customer.bike.price, customer.fund
+		)
+
+# Print the BikeShop again, now it's made a few sales...
+
+print shop
