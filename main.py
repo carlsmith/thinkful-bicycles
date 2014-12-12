1b5d0b805e485497be24c68dd4262035a88d37f3
Carl Smith <carl.input@gmail.com>
2014-12-12 19:56:15 +0000

    Sublime inserted tabs, not spaces - should be fixed now.

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from bicycles import Bicycle, BikeShop, Customer
 # with the Bikes...
 
 bikes = [
-	Bicycle("Rock Hopper", 75, 100), Bicycle("Dirt Jumper", 70, 150),
-	Bicycle("Speed Demon", 50, 250), Bicycle("Mountin Top", 90, 350),
-	Bicycle("Road Master", 65, 100), Bicycle("Ghetto King", 75, 550)
-	]
+    Bicycle("Rock Hopper", 75, 100), Bicycle("Dirt Jumper", 70, 150),
+    Bicycle("Speed Demon", 50, 250), Bicycle("Mountin Top", 90, 350),
+    Bicycle("Road Master", 65, 100), Bicycle("Ghetto King", 75, 550)
+    ]
 
 shop = BikeShop("Surplus Cycles", 20, bikes)
 
@@ -19,8 +19,8 @@ customers = [Customer("Ali", 200), Customer("Bob", 500), Customer("Caz", 1000)]
 
 for customer in customers:
 
-	bikes = ", ".join( bike.model for bike in shop.filter(customer.fund) )
-	print customer.name, "|", bikes
+    bikes = ", ".join( bike.model for bike in shop.filter(customer.fund) )
+    print customer.name, "|", bikes
 
 # Print the BikeShop before making any sales...
 
@@ -33,14 +33,14 @@ print shop
 template = "{0} bought the {1} at ${2}, and they have ${3} left over."
 
 for customer in customers:
-	
-	affordables = shop.filter(customer.fund)
-	shop.sell(random.choice(affordables), customer)
-	
-	print template.format(
-		customer.name, customer.bike.model,
-		customer.bike.price, customer.fund
-		)
+    
+    affordables = shop.filter(customer.fund)
+    shop.sell(random.choice(affordables), customer)
+    
+    print template.format(
+        customer.name, customer.bike.model,
+        customer.bike.price, customer.fund
+        )
 
 # Print the BikeShop again, now it's made a few sales...
 
